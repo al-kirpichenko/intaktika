7c057fb36f2aff96227f769573b3884b72ff38b1
al-kirpichenko <fuluganchik@yandex.ru>
2024-03-25 19:14:33 +0700

    Создан класс для работы с бд

--- DataBase.cs
+++ DataBase.cs
@@ -9,6 +9,27 @@ namespace intaktika
 {
     internal class DataBase
     {
+        SqlConnection conn = new SqlConnection(@"Data Source=PC;Initial Catalog=intaktika;Integrated Security=True");
 
+        public void openConnection()
+        {
+            if(conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+
+        public SqlConnection getConnection() 
+        { 
+            return conn; 
+        }
     }
 }
